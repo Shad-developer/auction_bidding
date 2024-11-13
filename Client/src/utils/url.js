98d35aaa365525078bc46b86f1572625e32bde68
Shad-developer <muhammadumairshad22@gmail.com>
2024-11-13 07:07:04 +0500

    update url

--- Client/src/utils/url.js
+++ Client/src/utils/url.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://localhost:5000/api";
+export const BACKEND_URL = "https://auction-bidding-coh9.onrender.com/api";
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(errorHandler);
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://auction-bidding-coh9.onrender.com",
     credentials: true,
   })
 );
